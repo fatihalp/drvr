9b62a975232220f62f8f7582aac2c4c289526b40
alp <alp@ankageo.com>
2016-10-09 16:08:56 +0300

    ilk

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+/*Driver App Configuration*/
+
+var krms_driver_config ={	
+	'ApiUrl':"http://posbee.co.uk/demo/driver/api",
+	'DialogDefaultTitle':"Posbee Driver",
+	'PushProjectID':"392505290000",
+	'APIHasKey':""
+};
